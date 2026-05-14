9a0a0799b0bb97429cf3c7ccbc33eff642b1fc96
Salil Thakur <salilthakur09@gmail.com>
2026-05-14 22:15:45 +0530

    oops 2

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,4 +1,4 @@
-class Student{
+/*class Student{
     String name;
     int age;
 
@@ -15,4 +15,36 @@ class Main{
         s.age=21;
         s.display();
     }
+}*/
+
+
+
+/*class Student{
+    String name;
+    int age;
+
+    void display(){
+        System.out.println(name);
+        System.out.println(age);
+    }
 }
+
+class Main{
+    public static void main(String args[]){
+        Student s1=new Student();
+        Student s2=s1;
+        s1.name="Salil";
+        s1.age=21;
+        s1.display();
+        if(s1==s2){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+        if(s1.equals(s2)){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+}*/
