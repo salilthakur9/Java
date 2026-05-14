e44058231007cee1a2d0ff8525ea6829c58e6501
Salil Thakur <salilthakur09@gmail.com>
2026-05-15 02:01:32 +0530

    practice 1

---
+++ OOPS/Practice.java
@@ -0,0 +1,37 @@
+/*import java.util.ArrayList;
+class Student{
+    private int id;
+    private String name;
+    private int age;
+    static int count=0;
+
+    Student(int id, String name, int age){
+        this.name=name;
+        this.id=id;
+        this.age=age;
+        count+=1;
+    }
+
+    void display(){
+        System.out.println(name+" of age "+age+" have id: "+id);
+    }
+
+    int get_id(){
+        return id;
+    }
+}
+
+class Practice {
+    public static void main(String args[]){
+        ArrayList<Student> students=new ArrayList<>();
+        students.add(new Student(1, "a",21));
+        students.add(new Student(2, "b",22));
+        students.add(new Student(3, "c",23));
+
+        for(Student s:students){
+            s.display();
+            System.out.println(s.get_id());
+        }
+        System.out.println("final count: "+ Student.count);
+    }
+}*/
