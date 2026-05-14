18b71b473a6235b179efca46bdbd9acb125cc195
Salil Thakur <salilthakur09@gmail.com>
2026-05-14 23:27:13 +0530

    oops 5

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -99,3 +99,28 @@ class Main{
         e.display();
     }
 }*/
+
+
+
+/*import java.util.Scanner;
+class Rectangle{
+    int length;
+    int breadth;
+
+    int area(){
+        return length*breadth;
+    }
+}
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+
+        Rectangle r=new Rectangle();
+        r.length=l;
+        r.breadth=b;
+        System.out.println(r.area());
+
+    }
+}*/
