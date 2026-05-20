c7066d034eacb16b0bd4224467ea1f9f32bed260
Salil Thakur <salilthakur09@gmail.com>
2026-05-20 16:42:58 +0530

    practice6

--- OOPS/Practice.java
+++ OOPS/Practice.java
@@ -223,3 +223,33 @@ class Practice{
         a.display();
     }
 }*/
+
+
+
+/*interface Shape{
+    void area();
+    void perimeter();
+}
+class Circle implements Shape{
+    double radius;
+    Circle(double radius){
+        this.radius=radius;
+    }
+    @Override
+    public void area() {
+        double area=Math.PI*radius*radius;
+        System.out.println("radius: "+radius);
+    }
+    @Override
+    public void perimeter(){
+        double perimeter=2*Math.PI*radius;
+        System.out.println("perimeter: "+perimeter);
+    }
+}
+class Practice{
+    public static void main(String args[]){
+        Shape s=new Circle(200);
+        s.area();
+        s.perimeter();
+    }
+}*/
